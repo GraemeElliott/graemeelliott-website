38f721f570c7e9400ca12e65f936bcf8aadfe8b7
Graeme Elliott <graeme.elliott@dimedout.com>
2022-10-07 23:49:34 +0100

    fixed some styling issues. Work started on homepage

--- src/components/partials/header/header.jsx
+++ src/components/partials/header/header.jsx
@@ -3,35 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="" variant="light">
       <Container>
-        <Navbar.Brand href="#home"></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+          <Nav className="me-auto mx-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/blog">Blog</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/views/homepage/homepage.jsx
+++ src/views/homepage/homepage.jsx
@@ -1,8 +1,16 @@
+import './homepage.scss';
+
 export default function Homepage() {
   return (
-    <div>
-      <h2> Homepage </h2>
-      <p> Some text </p>
+    <div className="home-container">
+      <div className="background-image-top"></div>
+      <div className="hero">
+        <h2>Graeme Elliott</h2>
+        <h3>Brand Protection Specialist/</h3>
+        <h3>Subjec Matter Expert</h3>
+        <div>Social Media Icons</div>
+        <div>Font Awesome Down Button</div>
+      </div>
     </div>
   );
 }
