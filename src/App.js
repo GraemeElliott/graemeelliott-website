868e907b64a17755fd578551ae7cc448170c2bf3
Graeme Elliott <graeme.elliott@dimedout.com>
2022-11-11 19:02:39 +0000

    Added new Google Tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
 import Homepage from './views/homepage/homepage.jsx';
 import Blog from './views/blog/blog';
 import BlogPostView from './views/blog/post-view/post-view';
@@ -10,14 +8,7 @@ import Footer from './components/partials/footer/footer';
 import BlogByCategory from './views/blog/blog-by-category/blog-by-category';
 import ScrollToTop from './components/partials/scroll-to-top/scroll-to-top';
 
-const TRACKING_ID = 'G-856S4BKHJ3';
-
-ReactGA.initialize(TRACKING_ID);
-
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return (
     <BrowserRouter>
       <ScrollToTop />
