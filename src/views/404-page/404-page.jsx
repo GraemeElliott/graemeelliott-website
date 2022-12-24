f26c0b91f0acc1682a21407062be9532f6d202f2
Graeme Elliott <graeme.elliott@dimedout.com>
2022-12-24 14:22:37 +0000

    Minor styling changes

--- src/views/404-page/404-page.jsx
+++ src/views/404-page/404-page.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default function NotFound404() {
   return (
     <div className="error-page-container">
-      <div className="text-container">
-        <h1 className="page-title"> 404 </h1>
-        <h2 className="page-subtitle">Page does not exist</h2>
-        <p>
+      <div className="error-page-text-container">
+        <h1 className="error-page-title"> 404 </h1>
+        <h2 className="error-page-subtitle">Page does not exist</h2>
+        <p className="error-page-text">
           The page you are looking for does not exist. How you got here is a
           mystery. But you can click the button below to go back.
         </p>
