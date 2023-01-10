39489b7b15b3128fd99f4d7fa5f83c77b90137f8
Graeme Elliott <graeme.elliott@dimedout.com>
2023-01-10 01:58:14 +0000

    Last Sanity v3 upgrades

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -2,16 +2,40 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { codeInput } from '@sanity/code-input';
 import { visionTool } from '@sanity/vision';
-import { schemaTypes } from './schema/schema';
+import schemas from './schemas/schema';
 
 export default defineConfig({
-  basePath: '/studio',
   name: 'graemeelliott-website',
   title: 'Graeme Elliott Website',
   projectId: 'lg88tis5',
   dataset: 'production',
   plugins: [deskTool(), visionTool(), codeInput()],
+  tools: (prev) => {
+    // 👇 Uses environment variables set by Vite in development mode
+    if (import.meta.env.DEV) {
+      return prev;
+    }
+    return prev.filter((tool) => tool.name !== 'vision');
+  },
   schema: {
-    types: schemaTypes,
+    types: schemas,
+  },
+  document: {
+    newDocumentOptions: (prev, { creationContext }) => {
+      if (creationContext.type === 'global') {
+        return prev.filter(
+          (templateItem) => templateItem.templateId != 'settings'
+        );
+      }
+      return prev;
+    },
+    actions: (prev, { schemaType }) => {
+      if (schemaType === 'settings') {
+        return prev.filter(
+          ({ action }) => !['unpublish', 'delete', 'duplicate'].includes(action)
+        );
+      }
+      return prev;
+    },
   },
 });
---
+++ studio/schemas/settings.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'settings',
+  title: 'Settings',
+  type: 'document',
+  fields: [
+    // ...all the fields
+  ],
+};
