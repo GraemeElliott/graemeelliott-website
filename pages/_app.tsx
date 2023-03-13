245f5c3308a15eeae968a16eb9e467ec5cb96408
Graeme Elliott <graeme.elliott@dimedout.com>
2023-03-13 23:38:43 +0000

    Adding favicon files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,16 +23,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className={theme}>
         <Navbar theme={theme} setTheme={setTheme} />
         <Container>
-          <Script
-            strategy="lazyOnload"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}`}
-          />
-          <Script strategy="lazyOnload">
-            {`window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}');`}
-          </Script>
           <Component {...pageProps} theme={theme} />
         </Container>
         <Footer />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="../../public/favicon.ico" />
-      </Head>
+      <Head></Head>
       <Main />
       <NextScript />
     </Html>
