aa4926402651e1581aef3a577e153736e5a4cb1d
Graeme Elliott <graeme.elliott@dimedout.com>
2023-05-30 17:40:29 +0100

    Updated About Me bio to be shorter

--- components/Homepage/AboutMe.tsx
+++ components/Homepage/AboutMe.tsx
@@ -15,25 +15,6 @@ export default function AboutMe() {
             start-up environment.
           </p>
           <p>
-            I have over eight years of experience developing and implementing
-            successful global brand protection programs for clients to help
-            protect their brand value online. I have honed my skills in
-            analysing, researching and mitigating potential security risks,
-            identifying counterfeit products, and uncovering details about bad
-            actors behind fraudulent activities. I have also produced
-            investigative reports into brand protection issues such as the sale
-            of counterfeit goods, websites phishing for user details,
-            unauthorised use of branding and sale of goods, and internet piracy.
-            My research findings have previously been distributed in the media.
-          </p>
-          <p>
-            As a proactive leader, I excel in collaborating with clients to
-            understand their issues and provide analysis and commentary while
-            guiding them through problem-solving strategies. I am skilled in
-            building enduring relationships with customers and clients whilst
-            maintaining existing services and seeking new growth opportunities.
-          </p>
-          <p>
             As a Product Owner, I have led cross-functional teams and
             stakeholders throughout the product development lifecycle. I have a
             deep understanding of Agile processes and principles. I use my
@@ -49,14 +30,6 @@ export default function AboutMe() {
         </div>
         <div className="about-me-right">
           <p>
-            Throughout my product career, I have helped establish clear product
-            visions to increase product engagement and usability, help define
-            areas for creating efficiencies, and position products to increase
-            sales, client growth and market share. This has involved creating
-            and prioritising backlogs that align with business and product
-            goals.
-          </p>
-          <p>
             With my technical understanding and expertise in product management,
             I am able to bridge the gap between business goals and technology
             solutions to ensure maximum value is achieved. I am passionate about
@@ -64,20 +37,10 @@ export default function AboutMe() {
             and contribute to overall organisational growth.
           </p>
           <p>
-            Another keen interest of mine is data gathering, analysis, and
-            visualisation. I often use these skills to create dashboards and
-            reports that provide insights into trends that help support my
-            team’s and clients’ decision-making.
-          </p>
-          <p>
             In my spare time, I am a web development hobbyist, learning web
-            development technologies such as JavaScript, React and Next JS
-            through online resources and textbooks. This has enabled me to gain
-            an in-depth understanding of how websites are structured. I enjoy
-            building user-friendly and responsive websites and learning more
-            about best web design practices with every project I undertake.
-            Whilst most of my projects are predominantly front-end, I have also
-            been learning back-end technologies such as Node.js.
+            development technologies such as JavaScript, React, Vue JS, Next JS
+            and Node Js through online resources and textbooks. This has enabled
+            me to gain an in-depth understanding of how websites are structured.
           </p>
           <p>
             Please get in touch with me via LinkedIn if you want to discuss my
